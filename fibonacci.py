0d5e8f914475f24a668351029ba24b9c0fa5cd57
Zayan770 <sasdaq@uoguelph.ca>
2025-10-21 10:44:09 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,3 +2,16 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
+while True:
+    num = input("Enter a positive integer: ")
+    if num.isdigit() and int(num) > 0:  # Check if input is a positive integer
+        num = int(num)
+        break   
+    else:
+        print("Invalid input. Please enter a positive integer.")
+
+a = 0
+b = 1
+for i in range(num):
+    print(a, end=' ' if i < num - 1 else '')
+    a, b = b, a + b
