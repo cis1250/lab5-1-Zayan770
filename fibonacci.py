471c93d21c14898320c0a5e0ad5eddad4950db9c
Zayan770 <sasdaq@uoguelph.ca>
2025-10-21 11:03:55 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,16 +2,30 @@
 
 # Fibonacci Sequence Exercise with functions
 # TODO: (Read detailed instructions in the Readme file)
-while True:
-    num = input("Enter a positive integer: ")
-    if num.isdigit() and int(num) > 0:  # Check if input is a positive integer
-        num = int(num)
-        break   
-    else:
-        print("Invalid input. Please enter a positive integer.")
+def get_positive_integer():
 
-a = 0
-b = 1
-for i in range(num):
-    print(a, end=' ' if i < num - 1 else '')
-    a, b = b, a + b
+    while True:
+        num = input("Please enter a positive integer: ")
+        if num.isdigit() and int(num) > 0:
+            return int(num)
+        else:
+            print("Invalid input. Try again.")
+
+def generate_fibonacci(n):
+    fib_sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_sequence.append(a)
+        a, b = b, a + b
+    return fib_sequence
+
+def print_fibonacci_sequence(seq):
+    print("Fibonacci sequence:")
+    for num in seq:
+        print(num, end=' ')
+    print()
+
+num_terms = get_positive_integer()
+fibonacci_sequence = generate_fibonacci(num_terms)
+print_fibonacci_sequence(fibonacci_sequence)
+
